62b6b6ae50ba2c1d98672fd5a2d99075200db570
Cezary Karabela <Cezary.Karabela@sollers.eu>
2017-11-16 09:45:37 +0100

    first

---
+++ gmaster-web-server/src/test/java/org/GMaster/gmasterwebserver/GmasterWebServerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.GMaster.gmasterwebserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmasterWebServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
