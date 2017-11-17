5d202b33c4291de52832b3a985804ddb5fe8323a
Alkhajit <c.karabela@outlook.com>
2017-11-17 17:34:42 +0100

    racelist

--- gmaster-web-frontend/src/app/app-routing.module.ts
+++ gmaster-web-frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppHome } from './app.home';
 import { HelloWorldComponent } from './hello-world/hello-world.component'
+import {RaceListComponent } from './race-list/race-list.component'
 
 const routes: Routes = [
-  {path:'helloworld', component: HelloWorldComponent}
+  {path:'helloworld', component: HelloWorldComponent},
+  {path:'races', component: RaceListComponent}
 ];
 
 @NgModule({
--- gmaster-web-frontend/src/app/app.module.ts
+++ gmaster-web-frontend/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HttpModule} from '@angular/http'
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http'
+import { HttpClientModule }from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppHome } from './app.home';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { RaceListComponent } from './race-list/race-list.component';
 
 @NgModule({
   declarations: [
     AppHome,
     HelloWorldComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    RaceListComponent
   ],
   imports: [
     BrowserModule,
-  AppRoutingModule],
+    AppRoutingModule,
+    HttpClientModule],
   providers: [],
   bootstrap: [AppHome]
 })
